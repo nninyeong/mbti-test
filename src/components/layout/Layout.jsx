1f79fee3845452be04c9117b5000a0fdd276159e
minyeong <minyk0512@gmail.com>
2024-09-08 00:43:30 +0900

    :tada: add: 레이아웃 기본 컴포넌트 구조 설정

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,17 @@
+import Header from './Header.jsx';
+import { Outlet } from 'react-router-dom';
+import Footer from './Footer.jsx';
+
+const Layout = () => {
+  return (
+    <>
+      <Header/>
+      <main>
+        <Outlet/>
+      </main>
+      <Footer/>
+    </>
+  );
+};
+
+export default Layout;
