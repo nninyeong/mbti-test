eb01965c52652361aeb8c14ffc255b9aa8dc2410
minyeong <minyk0512@gmail.com>
2024-09-09 01:56:13 +0900

    :lipstick: style: main 내부 요소들 크기가 작은 경우에도 전체 layout이 뷰포트 전체를 차지하도록 수정

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,13 +4,13 @@ import Footer from './Footer.jsx';
 
 const Layout = () => {
   return (
-    <>
+    <div className='flex flex-col h-full'>
       <Header />
-      <main className='flex justify-center mt-[50px] bg-gray-100'>
+      <main className='flex justify-center mt-[50px] bg-gray-100 flex-1'>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
