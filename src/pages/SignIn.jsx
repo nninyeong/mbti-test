c0764520b43232c4d693475b2bc3d8f5aa2c747d
minyeong <minyk0512@gmail.com>
2024-09-09 03:25:03 +0900

    :recycle: refactor: SignIn, SignUp의 공통 form 태그 AuthForm.jsx로 통일

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,8 +2,7 @@ import { useContext, useState } from 'react';
 import { login } from '../api/auth.js';
 import { userContext } from '../context/userContextProvider.jsx';
 import { Link, Navigate } from 'react-router-dom';
-import FormInput from '../components/Input/FormInput.jsx';
-import Button from '../components/Input/Button.jsx';
+import AuthForm from '../components/Input/AuthForm.jsx';
 
 const SignIn = () => {
   const [formValue, setFormValue] = useState({ id: '', password: '' });
@@ -12,7 +11,7 @@ const SignIn = () => {
   const [logedIn, setLogedIn] = useState(false);
   const handleSignIn = async (e) => {
     e.preventDefault();
-    const { accessToken, userId, success, avatar, nickname } = await login(formValue);
+    const { accessToken, success } = await login(formValue);
 
     let alertMessage;
     if (success) {
@@ -36,44 +35,23 @@ const SignIn = () => {
   }
 
   return (
-    <div className='flex flex-col justify-center items-center bg-white w-[500px] p-8 rounded shadow-md'>
+    <div className='flex flex-col justify-center items-center self-center gap-5 bg-white w-[500px] h-[400px] p-8 rounded shadow-md'>
       <h2 className='font-bold mb-2'>로그인</h2>
-      <form
-        className='border rounded p-5 max-w-[800px] bg-gray-200 flex flex-col justify-center items-center gap-5 py-[30px]'
+      <AuthForm
+        mode='signin'
         onSubmit={handleSignIn}
-      >
-        <FormInput
-          placeholder='아이디'
-          name='id'
-          type='text'
-          value={formValue}
-          setValue={setFormValue}
-          className='w-[300px]'
-        />
-        <FormInput
-          placeholder='비밀번호'
-          name='password'
-          type='password'
-          value={formValue}
-          setValue={setFormValue}
-          className='w-[300px]'
-        />
-        <Button
-          type='submit'
-          className='rounded w-full h-[35px] bg-primary-bg-red font-bold text-point-red'
+        formValue={formValue}
+        setFormValue={setFormValue}
+      />
+      <div className='flex gap-2'>
+        <p>계정이 없으신가요?</p>
+        <Link
+          to='/sign-up'
+          className='font-bold text-point-red'
         >
-          로그인
-        </Button>
-        <div className='flex gap-2'>
-          <p>계정이 없으신가요?</p>
-          <Link
-            to='/sign-up'
-            className='font-bold text-point-red'
-          >
-            회원가입
-          </Link>
-        </div>
-      </form>
+          회원가입
+        </Link>
+      </div>
     </div>
   );
 };
