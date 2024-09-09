da03ab143a8f1143873d5dc1dd0189f7ea168d13
minyeong <minyk0512@gmail.com>
2024-09-10 01:24:45 +0900

    :sparkles: feat: 테스트 결과에서 내 카드라면 공개 여부(isPublic)을 변경할 수 있는 버튼 렌더링, 버튼 클릭시 Optimistic Update를 사용해 UI에 먼저 반영되도록 함

--- src/components/mbtiTest/ResultCard.jsx
+++ src/components/mbtiTest/ResultCard.jsx
@@ -1,9 +1,33 @@
+import Button from '../Input/Button.jsx';
+import { useContext, useState } from 'react';
+import { userContext } from '../../context/userContextProvider.jsx';
+import { useToggleIsPublic } from '../../api/testResults.js';
+
 const ResultCard = ({ result }) => {
-  const { nickname, testResult, date } = result;
+  const { userProfile } = useContext(userContext);
+  const { nickname, testResult, date, isPublic, userId } = result;
+  const isMyResult = userProfile.userId === userId;
+
+  const { mutateIsPublic } = useToggleIsPublic();
+  const [isPublicCard, setIsPublicCard] = useState(isPublic);
+
+  const handleTogglePublic = () => {
+    setIsPublicCard(!isPublicCard);
+    mutateIsPublic(result);
+  };
   return (
     <div className='flex flex-col justify-center items-center w-[400px]'>
-      <div className='font-bold text-2xl text-white bg-point-red w-full h-[40px] text-center leading-[40px] rounded-t'>
+      <div className='flex flex-row justify-center items-center font-bold text-2xl text-white bg-point-red w-full h-[45px] rounded-t'>
         {nickname} 님
+        {isMyResult ? (
+          <Button
+            className='bg-primary-bg-red rounded p-1 h-[30px] font-medium text-lg text-gray-600'
+            type='button'
+            onClick={handleTogglePublic}
+          >
+            {isPublicCard ? '숨기기' : '공개하기'}
+          </Button>
+        ) : null}
       </div>
       <div className='flex flex-col justify-center items-start bg-white w-full p-5 rounded-b'>
         <div className='font-bold text-xl'>{testResult}</div>
